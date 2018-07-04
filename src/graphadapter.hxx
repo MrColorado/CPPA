8da21e3f7e4a84dd9e4c47a9173837cff9a57d65
MrColorado <theoissarni@hotmail.fr>
2018-07-04 23:32:53 +0200

    graphadapter: add main for test, fix constructor and domain

--- src/graphadapter.hxx
+++ src/graphadapter.hxx
@@ -1,50 +1,53 @@
-template <class T>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/zip.hpp>
+#include <range/v3/view/counted.hpp>
+
+template <typename T>
 Graphadapter<T>::Graphadapter(Graph graph)
+  : graph_(graph)
 {
-  graph_ = graph;
 }
 
-template <class T>
+template <typename T>
 const T& Graphadapter<T>::operator()(point_type p) const
 {
   return graph_[p];
 }
 
-template <class T>
+template <typename T>
 T& Graphadapter<T>::operator()(point_type p)
 {
   return graph_[p];
 }
 
-template <class T>
+template <typename T>
 auto Graphadapter<T>::domain() const
 {
-  auto tmp = 0;
-  return tmp;
+  return ranges::view::iota(0, num_vertices(graph_));
 }
 
-template <class T>
+template <typename T>
 auto Graphadapter<T>::values() const
 {
   auto tmp = 0;
   return tmp;
 }
 
-template <class T>
+template <typename T>
 auto Graphadapter<T>::values()
 {
   auto tmp = 0;
   return tmp;
 }
 
-template <class T>
+template <typename T>
 auto Graphadapter<T>::pixels() const
 {
   auto tmp = 0;
   return tmp;
 }
 
-template <class T>
+template <typename T>
 auto Graphadapter<T>::pixels()
 {
   auto tmp = 0;
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <boost/graph/graph_utility.hpp>
+#include "graphadapter.hh"
+
+enum { A, B, C, D, E, F, N };
+const char* name = "ABCDEF";
+typedef boost::adjacency_matrix<boost::undirectedS, boost::property<boost::vertex_index_t, unsigned int>> Graph;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+
+  Graph g(N);
+  add_edge(B, C, g);
+  add_edge(B, F, g);
+  add_edge(C, A, g);
+  add_edge(C, C, g);
+  add_edge(D, E, g);
+  add_edge(E, D, g);
+  add_edge(F, A, g);
+
+  std::cout << "vertex set: ";
+  boost::print_vertices(g, name);
+  std::cout << std::endl;
+
+  std::cout << "edge set: ";
+  boost::print_edges(g, name);
+  std::cout << std::endl;
+
+  std::cout << "out-edges: " << std::endl;
+  boost::print_graph(g, name);
+  std::cout << std::endl;
+
+  Graphadapter<unsigned int> tmp(g);
+
+  for (unsigned int i : tmp.domain())
+    std::cout << i << std::endl;
+
+  return 0;
 }
