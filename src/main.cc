c801dae236f41557385f555a81ab139bc0d5a84c
MrColorado <theoissarni@hotmail.fr>
2018-07-07 19:40:21 +0200

    graphadapter: create vertproperties WIP

--- src/main.cc
+++ src/main.cc
@@ -4,13 +4,20 @@
 
 enum { A, B, C, D, E, F, N };
 const char* name = "ABCDEF";
-typedef boost::adjacency_matrix<boost::undirectedS, boost::property<boost::vertex_index_t, unsigned int>> Graph;
-
+//typedef boost::adjacency_matrix<boost::undirectedS, boost::property<boost::vertex_index_t, unsigned int>> Graph;
+typedef boost::adjacency_matrix<boost::undirectedS, Graphadapter<int>::VertexProperties> Graph;
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
 
-  Graph g(N);
+  Graphadapter<int>::VertexProperties test[] = {{0, 10}, {1, 10}, {2, 20}};
+  Graphadapter<int>::VertexProperties test1 = {0, 10};
+  Graphadapter<int>::VertexProperties test2 = {1, 10};
+  Graphadapter<int>::VertexProperties test3 = {2, 20};
+
+
+  Graph g;
+  /*
   add_edge(B, C, g);
   add_edge(B, F, g);
   add_edge(C, A, g);
@@ -18,6 +25,7 @@ int main() {
   add_edge(D, E, g);
   add_edge(E, D, g);
   add_edge(F, A, g);
+   */
 
   std::cout << "vertex set: ";
   boost::print_vertices(g, name);
@@ -33,8 +41,16 @@ int main() {
 
   Graphadapter<unsigned int> tmp(g);
 
-  for (unsigned int i : tmp.domain())
+  for (auto i : tmp.domain())
     std::cout << i << std::endl;
+
+  std::cout << std::endl;
+
+  for (auto i : tmp.values())
+    std::cout << i << std::endl;
+
+  //for (unsigned int i : tmp.values())
+  //  continue; //std::cout << i << std::endl;
 
   return 0;
 }
