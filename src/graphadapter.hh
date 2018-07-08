0786253a20b37984cefb75521a4a8e68ccd64754
MrColorado <theoissarni@hotmail.fr>
2018-07-08 17:50:09 +0200

    template: graphadapter now use templated graph

--- src/graphadapter.hh
+++ src/graphadapter.hh
@@ -1,32 +1,20 @@
 #pragma once
 
-#include "boost/graph/adjacency_matrix.hpp"
 #include "boost/graph/adjacency_list.hpp"
+#include "boost/graph/adjacency_matrix.hpp"
 
-template <typename T>
+template<typename Graph, typename T>
 class Graphadapter
 {
-public:
-
-  struct VertexProperties
-  {
-    unsigned pos_;
-    T value_;
-    VertexProperties(unsigned pos, T value)
-      : pos_(pos), value_(value)
-    {}
-  };
-
-  typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, VertexProperties> Graph;
   using point_type = typename boost::graph_traits<Graph>::vertex_descriptor;
   using value_type = T;
   using pixel_type = std::pair<point_type, value_type>;
   using reference = T&;
   using const_reference = const T&;
-
   using vertex_iter = typename boost::graph_traits<Graph>::vertex_iterator;
 
-  Graphadapter(Graph* graph);
+public:
+  explicit Graphadapter(Graph* graph);
   const_reference operator()(point_type p) const;
   reference operator()(point_type p);
 
--- src/graphadapter.hxx
+++ src/graphadapter.hxx
@@ -1,65 +1,70 @@
+#include <range/v3/view/counted.hpp>
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/zip.hpp>
-#include <range/v3/view/counted.hpp>
 
 #include <vector>
-#include <iostream>
 
-template <class T>
-Graphadapter<T>::Graphadapter(Graph* graph)
+template<typename Graph, typename T>
+Graphadapter<Graph, T>::Graphadapter(Graph* graph)
   : graph_(graph)
 {
 }
 
-template <class T>
-const T& Graphadapter<T>::operator()(point_type p) const
+template<typename Graph, typename T>
+const T&
+Graphadapter<Graph, T>::operator()(point_type p) const
 {
-  return (*graph_)[vertex(p, *graph_)].value_;
+  return (*graph_)[vertex(p, *graph_)];
 }
 
-template <class T>
-T& Graphadapter<T>::operator()(point_type p)
+template<typename Graph, typename T>
+T&
+Graphadapter<Graph, T>::operator()(point_type p)
 {
-  return (*graph_)[vertex(p, *graph_)].value_;
+  return (*graph_)[vertex(p, *graph_)];
 }
 
-template <class T>
-auto Graphadapter<T>::domain() const
+template<typename Graph, typename T>
+auto
+Graphadapter<Graph, T>::domain() const
 {
   auto first = *vertices(*graph_).first;
   auto second = *vertices(*graph_).second;
   return ranges::view::iota(first, second);
 }
 
-template <class T>
-auto Graphadapter<T>::values() const
+template<typename Graph, typename T>
+auto
+Graphadapter<Graph, T>::values() const
 {
   std::vector<point_type> tmp;
   std::pair<vertex_iter, vertex_iter> vp = boost::vertices(*graph_);
   for (; vp.first != vp.second; ++vp.first)
-    tmp.push_back((*graph_)[*vp.first].value_);
+    tmp.push_back((*graph_)[*vp.first]);
   return ranges::view::counted(tmp.begin(), tmp.size());
 }
 
-
-template <class T>
-auto Graphadapter<T>::values()
+template<typename Graph, typename T>
+auto
+Graphadapter<Graph, T>::values()
 {
   std::vector<point_type> tmp;
   std::pair<vertex_iter, vertex_iter> vp = boost::vertices(*graph_);
   for (; vp.first != vp.second; ++vp.first)
-    tmp.push_back((*graph_)[*vp.first].value_);
+    tmp.push_back((*graph_)[*vp.first]);
   return ranges::view::counted(tmp.begin(), tmp.size());
 }
 
-template <class T>
-auto Graphadapter<T>::pixels() const
+template<typename Graph, typename T>
+auto
+Graphadapter<Graph, T>::pixels() const
 {
   return ranges::view::zip(domain(), values());
 }
 
-template <class T>
-auto Graphadapter<T>::pixels()
+template<typename Graph, typename T>
+auto
+Graphadapter<Graph, T>::pixels()
 {
   return ranges::view::zip(domain(), values());
 }
